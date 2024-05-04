ee5fe130d43bf6b38e85b11ecf2a9fe0fbc2c188
JesusVA760 <tonio4340406@gmail.com>
2024-05-03 23:13:44 -0700

    hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "../Images/Hero.png";
 import "./Navbar.css";
+
 const Navbar = () => {
+  const [isHamMenuActive, setIsHamMenuActive] = useState(false);
+
+  function toggleHamMenu() {
+    console.log("Toggle hamburger menu");
+
+    setIsHamMenuActive(!isHamMenuActive);
+  }
+
   return (
     <div className="nav-container">
       <img src={Hero} alt="Hero" />
       <div className="navbar">
+        <div className={`ham-menu ${isHamMenuActive ? "active" : ""}`}>
+          <div className="off-screen-menu">
+            <a href="#page-1">About Me</a>
+            <a href="#page-2">Experience</a>
+            <a href="#page-3">Contact Me</a>
+          </div>
+        </div>
+
+        <nav className="ham-menu" onClick={toggleHamMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </nav>
         <nav className="navbar-options">
           <a className="home-button">Home</a>
           <a href="#page-1">About Me</a>
