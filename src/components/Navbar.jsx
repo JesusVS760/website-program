389a90c386d65bfe09a84282dbeabc336884d0dc
JesusVA760 <tonio4340406@gmail.com>
2024-05-03 23:38:34 -0700

    hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,27 +7,27 @@ const Navbar = () => {
 
   function toggleHamMenu() {
     console.log("Toggle hamburger menu");
-
     setIsHamMenuActive(!isHamMenuActive);
+    console.log("isHamMenuActive:", isHamMenuActive);
   }
 
   return (
     <div className="nav-container">
       <img src={Hero} alt="Hero" />
       <div className="navbar">
-        <div className={`ham-menu ${isHamMenuActive ? "active" : ""}`}>
-          <div className="off-screen-menu">
-            <a href="#page-1">About Me</a>
-            <a href="#page-2">Experience</a>
-            <a href="#page-3">Contact Me</a>
-          </div>
+        <div className={`off-screen-menu ${isHamMenuActive ? "active" : ""}`}>
+          <a className="home-button">Home</a>
+          <a href="#page-1">About Me</a>
+          <a href="#page-2">Experience</a>
+          <a href="#page-3">Contact Me</a>
         </div>
-
-        <nav className="ham-menu" onClick={toggleHamMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </nav>
+        <div className={`ham-menu ${isHamMenuActive ? "active" : ""}`}>
+          <nav className="ham-menu" onClick={toggleHamMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </nav>
+        </div>
         <nav className="navbar-options">
           <a className="home-button">Home</a>
           <a href="#page-1">About Me</a>
